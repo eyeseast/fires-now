8eeeabf7f25e471d3203a23cecdb33eac3f2c050
Glitch (eyeseast-fires-now) <none>
2019-10-31 13:52:21 +0000

    🍶🎻 Updated with Glitch

--- fires.js
+++ fires.js
@@ -1,29 +1,11 @@
 #!/usr/bin/env node
-/*
-Serve a page with fire data loaded
-*/
 
-const express = require("express");
 const fetch = require("node-fetch");
 const togeojson = require("@tmcw/togeojson");
 const { DOMParser } = require("xmldom");
 
-const app = express();
-
-const { PORT } = process.env;
 const ACTIVE_FIRES =
   "https://rmgsc.cr.usgs.gov/outgoing/GeoMAC/ActiveFirePerimeters.kml";
-
-app.use(express.static("./public"));
-
-app.get("/fires.json", async (req, res) => {
-  const fires = await getFires()
-  res.json(fires);
-});
-
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
-});
 
 async function getFires() {
   const kml = await fetch(ACTIVE_FIRES)
@@ -32,3 +14,11 @@ async function getFires() {
 
   return togeojson.kml(kml);
 }
+
+if (module === require.main) {
+  getFires()
+    .then(JSON.stringify)
+    .then(console.log);
+}
+
+module.exports = getFires;
